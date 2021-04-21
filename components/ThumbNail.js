766a753fdca8af89e1fe58738a22c262ee45c56f
Jashan Khela <jashan.khela@hotmail.com>
2021-04-21 00:01:36 -0700

    v1

---
+++ components/ThumbNail.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { ThumbsUpIcon } from '@heroicons/react/outline'
+import { forwardRef } from 'react';
+const ThumbNail = forwardRef(({result} , ref ) => {
+  const BASE_URL = "https://image.tmdb.org/t/p/original"
+  return (
+    <div ref={ref} className=' p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50 group cursor-pointer'>
+      <Image
+      layout='responsive'
+      src={
+        `${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`
+      }
+      height={1000}
+      width={1920}/>
+      <div className='p-2'>
+        <p className='truncate max-w-md'>{result.overview}</p>
+        <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold'>{result.title || result.original_name}</h2>
+        <p></p>
+      </div>
+    </div>
+  )
+})
+
+export default ThumbNail
